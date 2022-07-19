959b6108e81a79c39b556e90db602da5cc00b1f3
kinho <kinhong99@gmail.com>
2022-07-18 20:17:36 -0400

    Error handling in go

--- modules-in-go/greetings/greetings.go
+++ modules-in-go/greetings/greetings.go
@@ -1,8 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("name was empty")
+	}
+
 	message := fmt.Sprintf("Hi, %v Welcome to Go!", name)
-	return message
+	return message, nil
 }
--- modules-in-go/hello/hello.go
+++ modules-in-go/hello/hello.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Kin")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
