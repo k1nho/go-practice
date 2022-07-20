5a627fe2bbfe10f9ddfc11e101a7d7d7ca88d485
kinho <kinhong99@gmail.com>
2022-07-19 21:46:27 -0400

    more basics of Go

---
+++ gowiki/wiki.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
