51822480b2f0bbd101f26cc4e1fa393f5545f6e6
kinho <kinhong99@gmail.com>
2022-07-24 16:07:31 -0400

    structs, receivers, and mutations

--- goroutines/goroutinebasics.go
+++ goroutines/goroutinebasics.go
@@ -3,7 +3,26 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+// structs
+type Point struct {
+	X int
+	Y int
+}
+
+// Receivers (getters)
+func (p Point) Diff() int {
+	res := p.X - p.Y
+	return res
+}
+
+// mutations (setters)
+func (p *Point) Scale(factor int) {
+	p.X = p.X * factor
+	p.Y = p.Y * factor
+}
 
 func push(name string, ch chan string) {
 	msg := "Hey " + name
@@ -24,6 +43,19 @@ func main() {
 
 	integerSub()
 	runWaitGroup()
+	deferrer()
+
+	fmt.Println("Defining  a point")
+	p := Point{X: 2, Y: 2}
+	fmt.Printf("The x coordinate is %v and the y coordinate is %v\n", p.X, p.Y)
+	p.X = 5
+	fmt.Printf("Changing the code to assign x to a %v\n", p.X)
+	// lets use getters to get the difference
+	diff := p.Diff()
+	fmt.Printf("Difference between x and y is %v\n", diff)
+	p.Scale(2)
+	fmt.Printf("Doubling our coordinates we have x: %v, y: %v\n", p.X, p.Y)
+
 }
 
 func integerSub() {
@@ -59,4 +91,14 @@ func operate(wg *sync.WaitGroup, item string) {
 	defer wg.Done()
 	item = item + " this is item"
 	fmt.Println(item)
+}
+
+// Defers a function running until the surrounding function returns
+func deferrer() {
+	var d = int64(0)
+	defer func(d *int64) {
+		fmt.Printf(" & %v seconds", *d)
+	}(&d)
+	fmt.Print("Done\n")
+	d = time.Now().Unix()
 }
