a02e87d945a8648f33e66d0784a09aff7acb9d0b
kinho <kinhong99@gmail.com>
2022-07-23 23:48:50 -0400

    Using read/writes os mod

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -12,4 +13,20 @@ type Page struct {
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadChange(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "Go page", Body: []byte("Go main page")}
+	p1.save()
+	p2, _ := loadChange(p1.Title)
+	fmt.Println(string(p2.Body))
 }
