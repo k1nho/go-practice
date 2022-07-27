ce89d7f8c3cbd65bd54bf889670e4beca885f8f0
kinho <kinhong99@gmail.com>
2022-07-26 21:52:15 -0400

    gorilla mux

--- goGorilla/main.go
+++ goGorilla/main.go
@@ -1,13 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+
 
 func main() {
-	r := mux.newRouter()
+	r := mux.NewRouter()
 
-	r.handleFunc(
+	r.HandleFunc(
 		"/series/{title}/episode/{ep}", func(w http.ResponseWriter, r *http.Request) {
-			vars := mux.Vars(r)
+			vars := mux.Vars(r) // getting all params using Vars method
+			fmt.Printf("The title of the series is %v\n", vars["title"])
+			fmt.Printf("The episode of the series is %v\n", vars["ep"])
 		})
+
+	// since we are using a mux and not the default we need to pass it to the listen function
+	http.ListenAndServe(":80", r)
+
+	// Gorilla mux also allows us to restrict the methods used in this case we can use Methods() to specify which routes should respond
+	// only to an specific method for ex
+	r.HandleFunc("api/bookmark/{title}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Printf(" Bookmark the following title %v", vars["title"])
+	}).Methods("POST")
+	
+	// we can also restrict hosts i.e (www.bookstore.com) or restrict schmes (http/https)
+	func getAllCharacters(w http.http.ResponseWriter, r *http.Request) int {
+		return 1;	
+	}
+
+	func getCharacter(w http.ResponseWriter, r *http.Request){
+		
+	}
+	// To organize routes better we can have a common prefix 
+	characterRouter := r.PathPrefix("/character").Subrouter()
+	characterRouter.HandleFunc("/", getAllCharacters);
+	characterRouter.HandleFunc("/{id}", getCharacter);
 
 }
