6e98c91d8a9b8938eba2acec40e4d929f0e9ffcd
Kin N <kinn@ip-10-7-0-146.ec2.internal>
2022-08-04 14:16:01 -0400

    using read/write mutex to have multiple readers at the same time

--- goConcurrency/mutex.go
+++ goConcurrency/mutex.go
@@ -7,18 +7,25 @@ import (
 )
 
 var (
-	count int
-	lock  sync.Mutex
+	count  int
+	lock   sync.Mutex
+	rwlock sync.RWMutex
 )
 
 func main() {
 	numberOfRoutines := 10000
-
+	numberOfReaders := 5
 	for i := 0; i < numberOfRoutines; i++ {
 		go increaseCount()
 	}
 
-	time.Sleep(time.Second * 1)
+	go writer()
+	// we can also use a read/write lock to be able to access the critical section by many readers but only one writer
+	for i := 0; i < numberOfReaders; i++ {
+		go reader()
+	}
+
+	time.Sleep(time.Second * 3)
 	fmt.Printf("The count is %v", count)
 }
 
@@ -26,4 +33,20 @@ func increaseCount() {
 	lock.Lock()
 	count++ // this operation is not atomic, since it actually performs a reassignement to get the correct result
 	lock.Unlock()
+}
+
+func reader() {
+	rwlock.RLock()
+	defer rwlock.RUnlock()
+	fmt.Println("Reader acquire lock")
+	time.Sleep(time.Second * 1)
+	fmt.Println("Reader release lock")
+}
+
+func writer() {
+	rwlock.Lock()
+	defer rwlock.Unlock()
+	fmt.Println("Writer acquire lock")
+	time.Sleep(time.Second * 1)
+	fmt.Println("Writer release lock")
 }
