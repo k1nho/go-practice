c4bf07488dba87e678f4faefc3223b76d2771d3c
kinho <kinhong99@gmail.com>
2022-08-03 21:14:31 -0400

    Using the close method to signal to a channel that we are done

--- goConcurrency/channelsIteration.go
+++ goConcurrency/channelsIteration.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	channel := make(chan string)
+	channel2 := make(chan string)
 	n := 3
 
 	// start concurrent routine
@@ -14,10 +15,24 @@ func main() {
 	for i := 0; i < n; i++ {
 		fmt.Println(<-channel)
 	}
+
+	go throwBall(channel2, n)
+	for message := range channel2 {
+		fmt.Println(message)
+	}
 }
 
 func shootBall(channel chan string, rounds int) {
 	for i := 0; i < rounds; i++ {
 		channel <- fmt.Sprint("Shoot ball!")
 	}
+}
+
+func throwBall(channel chan string, rounds int) {
+	for i := 0; i < rounds; i++ {
+		channel <- fmt.Sprint("Throw ball!")
+	}
+
+	// using close method to signal to the channel that we have finished our processes
+	close(channel)
 }
