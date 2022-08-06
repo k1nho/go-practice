fc6844b46f8c3a18bfaa6aa6ab022b6c3ed03c79
kinho <kinhong99@gmail.com>
2022-08-05 23:35:24 -0400

    atomic instead of mutex

--- goConcurrency/atomic.go
+++ goConcurrency/atomic.go
@@ -2,6 +2,30 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var (
+	count int32
+)
+
 func main() {
 	// setup
+	workers := 100000000
+
+	var wg sync.WaitGroup
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		go increaseCount(&wg)
+	}
+	wg.Wait()
+	fmt.Printf("The value of count is %d", count)
+}
+
+func increaseCount(wg *sync.WaitGroup) {
+	atomic.AddInt32(&count, 1)
+	wg.Done()
 }
