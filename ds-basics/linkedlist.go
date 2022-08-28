5db820a2169ba20e41401b4e55cd32b2f5c791ea
kinho <kinhong99@gmail.com>
2022-08-27 21:34:57 -0400

    remove and size

--- ds-basics/linkedlist.go
+++ ds-basics/linkedlist.go
@@ -8,26 +8,25 @@ type Node struct {
 
 type LinkedList struct { 
 	head *Node
+	tail *Node
 	size int
 }
 
-func (l *LinkedList) add(x int){
+func (l *LinkedList) Add(x int){
 	newNode := new(Node)
 	newNode.val = x
+	l.size++
 
 	if l.head == nil {
-		l.head = newNode	
+		l.head, l.tail = newNode	
 	}
 	else{
-		it := l.head
-		for ; it.next != nil; it = it.next{
-		}
-		it.next = newNode
+		l.tail.next = newNode
 	}
 }
 
-func (l *LinkedList) get(x int) *Node{
-	for it := l.head; it.next != nil; it = it.next{
+func (l LinkedList) Get(x int) *Node{
+	for it := l.head; it != nil; it = it.next{
 		if it.val == x {
 			return it
 		}
@@ -35,28 +34,42 @@ func (l *LinkedList) get(x int) *Node{
 	return nil
 }
 
-func (l LinkedList) remove(x int){
+func (l *LinkedList) Remove(x int){
+	var prev *Node
+	
+	for cur := l.head; cur != nil; cur = cur.next {
 
+		if cur.val == x {
+			if l.head == cur {
+				l.head = cur.next
+				if l.size == 0 {
+					l.tail = cur.next
+				}
+			} else if l.tail == cur{
+				l.tail = prev
+				prev.next = cur.next
+			} else{
+				prev.next = cur.next
+			}
+			return
+		}
+		prev = cur
+	}
+	l.size--
 }
 
+
+func (l LinkedList) Size() int {
+	return l.size
+}
 
 func (l LinkedList) String() string{
 	sb := strings.Builder{}
 
-	for it := l.head; it.next != nil; it = it.next{
+	for it := l.head; it != nil; it = it.next{
 		sb.WriteString(fmt.Sprintf("%d ", it.val))
 	}
 
 	return sb.String()
-
-}
-
-
-func main() {
-	LL := LinkedList{}
-	LL.add(1)
-	LL.add(2)
-	LL.add(3)
-	LL.add(4)
 
 }
