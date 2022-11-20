2f006764c9cc975b3b6893eaca007ec83fa89eb7
k1nho <kinhong99@gmail.com>
2022-11-19 21:30:34 -0500

    interfaces cont

--- exercises/interfaces.go
+++ exercises/interfaces.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+  "strconv"
 )
 
 type ShapeAreaRes interface {
@@ -44,6 +45,20 @@ func checkType(shape Shape) {
   fmt.Printf("Type of this is %T\n", shape)
 }
 
+
+// Stringers ex
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+  var s string
+  for _ , val := range ip {
+    s += strconv.Itoa(int(val)) + "."
+  }
+
+  return s[:len(s)-1]
+}
+
+
 func main() {
   var shape Shape
 
@@ -59,4 +74,13 @@ func main() {
   // Stringer interface is used to implement print of values to strings
   rect := Rectangle{10,40}
   fmt.Println(rect)
+
+  hosts := map[string]IPAddr{
+    "loopback": {127,0,0,1},
+    "googleDNS" :{8,8,8,8},
+  }
+
+  for k,v := range hosts {
+    fmt.Printf("%v, %v\n", k, v)
+  }
 }
