bb9b5efa7745ab3c9efbe52c8b4f05e3f80b669f
k1nho <kinhong99@gmail.com>
2022-11-20 16:12:52 -0500

    patterns and ex

--- exercises/concurrency.go
+++ exercises/concurrency.go
@@ -7,6 +7,12 @@ import (
 )
 
 
+type Tree struct {
+  Left *Tree
+  Right *Tree
+  Value int
+}
+
 func printMessage(wg *sync.WaitGroup) {
   fmt.Println("here is the message")
   wg.Done()
@@ -25,7 +31,60 @@ func main() {
   
   wg.Wait()
   
+  fmt.Println("for select pattern")
+  forSelectPattern()
+
+  ch := make(chan int)
+  root := Tree{
+    Left: &Tree{nil, nil, 0},
+    Right: &Tree{nil, nil, 2},
+    Value : 1,
+  }
+
+  go func(){
+    Walk(&root, ch)
+    close(ch)
+  }()
+
+  for i := range ch {
+    fmt.Printf("%d ", i)
+  }
+
+  
   fmt.Printf("time passed %v\n", time.Since(t))
-  fmt.Println("main finish")
+  fmt.Println("Finish")
 }
+
+func Walk(t *Tree, ch chan int) {
+  if t == nil {
+    return
+  }
+  Walk(t.Left, ch)
+  ch <- t.Value
+  Walk(t.Right, ch)
+}
+
+
+// patterns
+
+// 1. for-select loop (buffered channels)
+// make go routine queue up results in a channel
+func forSelectPattern() {
+  channel := make(chan string, 3)
+  leaders := []string{"Brock", "Blaine", "Cynthia"}
+
+  for _, leader := range leaders {
+    select {
+      case channel <- leader:
+   }
+  }
+
+  close(channel)
+
+  for data := range channel {
+    fmt.Printf("Leader obtained %s\n", data)
+  }
+  
+}
+
 
