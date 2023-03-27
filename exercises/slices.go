3f4546110c669d590bac53ecd11f453d3dc8b515
k1nho <kinhong99@gmail.com>
2023-03-27 19:41:22 -0400

    slices

--- exercises/slices.go
+++ exercises/slices.go
@@ -1,30 +1,29 @@
 package main
 
-
 func pix(x, y, mode int) int {
-  switch mode {
-    case 1:
-      return x^y
-    case 2:
-      return (x+y)/2
-    default:
-      return x*y
-  }
+	switch mode {
+	case 1:
+		return x ^ y
+	case 2:
+		return (x + y) / 2
+	default:
+		return x * y
+	}
 }
 
 func Pic(dx, dy int) [][]uint8 {
-  grid := make([][]uint8 , dy)  
+	grid := make([][]uint8, dy)
 
-  for i:= range grid {
-    grid[i] = make([]uint8, dx)
-    for j:= range grid[i] {
-      grid[i][j] = uint8(pix(i, j, 1))
-    }
-  }
+	for i := range grid {
+		grid[i] = make([]uint8, dx)
+		for j := range grid[i] {
+			grid[i][j] = uint8(pix(i, j, 1))
+		}
+	}
 
-  return grid;
+	return grid
 }
 
 func main() {
-  pic.Show(Pic)
+	pic.Show(Pic)
 }
